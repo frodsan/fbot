260d8aaf67a922d87cc11271c95408e28ad3d6b2
Francesco Rodríguez <frodsan@protonmail.ch>
2016-05-11 13:55:35 +0200

    Test event triggering.

--- events_test.go
+++ events_test.go
@@ -5,9 +5,23 @@ import "testing"
 func TestPanicIfEventNameIsInvalid(t *testing.T) {
 	defer func() {
 		if r := recover(); r == nil {
-			t.Errorf("Given event name must be invalid")
+			t.Error("Given event name must be invalid")
 		}
 	}()
 
 	On("invalid", func(_ Event) {})
+}
+
+func TestOKEventTrigger(t *testing.T) {
+	var ok bool
+
+	On("message", func(_ Event) {
+		ok = true
+	})
+
+	trigger("message", Event{})
+
+	if !ok {
+		t.Error("Event must be called")
+	}
 }
