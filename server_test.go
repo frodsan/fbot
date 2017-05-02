dfd6aa9441de902087f062120a3790a024240dce
Francesco Rodriguez <frodsan@protonmail.com>
2017-05-02 23:40:20 +0200

    Add more tests to webhook verification process.

--- server_test.go
+++ server_test.go
@@ -28,7 +28,7 @@ func TestMethodNotAllowed(t *testing.T) {
 }
 
 func TestWrongVerifyMode(t *testing.T) {
-	queryParams := url.Values{"mode": {"invalid"}}
+	queryParams := url.Values{"hub.mode": {"invalid"}}
 
 	req, error := http.NewRequest("GET", "/webhook?"+queryParams.Encode(), nil)
 
@@ -46,5 +46,63 @@ func TestWrongVerifyMode(t *testing.T) {
 
 	if status := res.Code; status != http.StatusForbidden {
 		t.Errorf("Expected status code %v; got %v", http.StatusForbidden, status)
+	}
+}
+
+func TestWrongVerifyToken(t *testing.T) {
+	queryParams := url.Values{
+		"hub.mode":         {"subscribe"},
+		"hub.verify_token": {"wrong"},
+	}
+
+	req, error := http.NewRequest("GET", "/webhook?"+queryParams.Encode(), nil)
+
+	if error != nil {
+		t.Fatal(error)
+	}
+
+	bot := NewBot()
+
+	res := httptest.NewRecorder()
+
+	handler := Handler(bot)
+
+	handler.ServeHTTP(res, req)
+
+	if status := res.Code; status != http.StatusForbidden {
+		t.Errorf("Expected status code %v; got %v", http.StatusForbidden, status)
+	}
+}
+
+func TestOKVerifyToken(t *testing.T) {
+	queryParams := url.Values{
+		"hub.mode":         {"subscribe"},
+		"hub.verify_token": {"ok"},
+		"hub.challenge":    {"challenge"},
+	}
+
+	req, error := http.NewRequest("GET", "/webhook?"+queryParams.Encode(), nil)
+
+	if error != nil {
+		t.Fatal(error)
+	}
+
+	bot := NewBot()
+	bot.VerifyToken = "ok"
+
+	res := httptest.NewRecorder()
+
+	handler := Handler(bot)
+
+	handler.ServeHTTP(res, req)
+
+	if status := res.Code; status != http.StatusOK {
+		t.Errorf("Expected status code %v; got %v", http.StatusOK, status)
+	}
+
+	expected := queryParams["hub.challenge"][0]
+
+	if string(res.Body.Bytes()) != expected {
+		t.Errorf("Expected %s, got %s", expected, res.Body.Bytes())
 	}
 }
