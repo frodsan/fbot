8c60701c6080006091545a5f7691cdc357ef6bab
Francesco Rodriguez <frodsan@protonmail.com>
2017-05-02 22:31:15 +0200

    Minor change on private method name.

--- server.go
+++ server.go
@@ -9,14 +9,14 @@ func Handler(bot Bot) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case "GET":
-			checkToken(bot, w, r)
+			validateWebhook(bot, w, r)
 		default:
 			w.WriteHeader(http.StatusMethodNotAllowed)
 		}
 	}
 }
 
-func checkToken(bot Bot, w http.ResponseWriter, r *http.Request) {
+func validateWebhook(bot Bot, w http.ResponseWriter, r *http.Request) {
 	mode := r.FormValue("hub.mode")
 	token := r.FormValue("hub.verify_token")
 
