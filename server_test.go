1ac3419e3807eea7b1c1070f37c501557911a828
Francesco Rodriguez <frodsan@protonmail.com>
2017-05-02 23:25:18 +0200

    Add test for wrong verification mode.

--- server_test.go
+++ server_test.go
@@ -3,25 +3,48 @@ package fbot
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
 func TestMethodNotAllowed(t *testing.T) {
-	req, err := http.NewRequest("HEAD", "/", nil)
+	req, eres := http.NewRequest("HEAD", "/webhook", nil)
 
-	if err != nil {
-		t.Fatal(err)
+	if eres != nil {
+		t.Fatal(eres)
 	}
 
 	bot := NewBot()
 
-	rr := httptest.NewRecorder()
+	res := httptest.NewRecorder()
 
 	handler := Handler(bot)
 
-	handler.ServeHTTP(rr, req)
+	handler.ServeHTTP(res, req)
 
-	if status := rr.Code; status != http.StatusMethodNotAllowed {
+	if status := res.Code; status != http.StatusMethodNotAllowed {
 		t.Errorf("Expected status code %v; got %v", http.StatusMethodNotAllowed, status)
+	}
+}
+
+func TestWrongVerifyMode(t *testing.T) {
+	queryParams := url.Values{"mode": {"invalid"}}
+
+	req, eres := http.NewRequest("GET", "/webhook"+queryParams.Encode(), nil)
+
+	if eres != nil {
+		t.Fatal(eres)
+	}
+
+	bot := NewBot()
+
+	res := httptest.NewRecorder()
+
+	handler := Handler(bot)
+
+	handler.ServeHTTP(res, req)
+
+	if status := res.Code; status != http.StatusForbidden {
+		t.Errorf("Expected status code %v; got %v", http.StatusForbidden, status)
 	}
 }
